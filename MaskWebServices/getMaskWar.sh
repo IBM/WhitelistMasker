4cafc19259c87c0c21b86ff277b320da29040c86
Nathaniel Mills <wnm3@us.ibm.com>
2020-06-10 12:14:55 -0400

    IBM/WhitelistMasker#5

--- MaskWebServices/getMaskWar.sh
+++ MaskWebServices/getMaskWar.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cp -r ~/OSS/WhitelistMasker/WhitelistMasker/MaskWebServices/target/MaskWebServices-1.0.0.war ./dropins/.
+cp -r ~/OSS/WhitelistMasker/WhitelistMasker/MaskWebServices/target/MaskWebServices-1.0.1.war ./dropins/.
