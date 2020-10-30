12d67698602df18844188c313a19d0a94c217a1a
Nathaniel Mills <wnm3@us.ibm.com>
2020-10-30 15:48:53 -0400

    IBM/WhitelistMasker#13

--- MaskWebServices/removeMaskerWebServices.sh
+++ MaskWebServices/removeMaskerWebServices.sh
@@ -4,5 +4,3 @@ docker container stop maskersvcs
 docker container rm -f maskersvcs
 docker image rm -f maskerwebservices
 rm -f maskersvcs.tar
-rm -f maskerwebservices.tar
-rm -f maskerwebservices.tar.gz
