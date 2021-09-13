f7a56ee893788a31a10a6100a352c51eb1bb7b82
wnmills3 <wnmills3@gmail.com>
2021-09-13 12:27:31 -0400

    IBM/WhitelistMasker#27

--- MaskWebServices/loadMaskerWebServices.sh
+++ MaskWebServices/loadMaskerWebServices.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 cp ./maskerwebservices.tar.gz ./maskersvcs.tar.gz
-gunzip maskersvcs.tar.gz 
-docker load -i maskersvcs.tar 
-if [ ! -d "/store/WAALogs" ] 
+docker load -i maskersvcs.tar.gz 
+if [ ! -d "/store/WAALogs/maskersvcs" ] 
 then
-   mkdir /store/WAALogs
+   mkdir -p /store/WAALogs/maskersvcs
 fi
 if [ ! -d "/store/WAAData/maskersvcs/properties" ] 
 then
    docker run  --detach --name maskersvcs -p 9080:9080 maskerwebservices
 else
-   docker run -v /store/WAALogs:logs -v /store/WAAData/maskersvcs/properties:/opt/ol/wlp/output/defaultServer/properties --detach --name maskersvcs -p 9080:9080 -p 9980:9980 maskerwebservices
+   docker run -v /store/WAALogs/maskersvcs:/logs -v /store/WAAData/maskersvcs/properties:/opt/ol/wlp/output/defaultServer/properties --detach --name maskersvcs -p 9080:9080 -p 9980:9980 maskerwebservices
 fi
 sleep 10
 docker logs maskersvcs
